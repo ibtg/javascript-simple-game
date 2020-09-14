a106c9f0923fa28570d8b4dcf015777fc2ae43f9
ibtg <ibtg0409@gmail.com>
2020-09-14 11:51:20 +0900

    Modify class name

--- src/popup.js
+++ src/popup.js
@@ -17,10 +17,10 @@ export default class PopUp {
 
   showWithText(text) {
     this.popUpText.innerText = text;
-    this.popUp.classList.remove('pop-up--hide');
+    this.popUp.classList.remove('pop-up__hide');
   }
 
   hide() {
-    this.popUp.classList.add('pop-up--hide');
+    this.popUp.classList.add('pop-up__hide');
   }
 }
